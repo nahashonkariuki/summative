0a3ce3cff2d9aeefc6878bc3daa04f5215f62c35
nahashonkariuki <nahashonkariuki@users.noreply.github.com>
2022-04-16 13:41:06 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="nahashonkariuki"
+original_name="summative"
+original_urlname="summative"
+original_description="Awesome summative created by nahashonkariuki"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv summative $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project_name import BaseClass, base_function
+from summative import BaseClass, base_function
 
 given = pytest.mark.parametrize
 
